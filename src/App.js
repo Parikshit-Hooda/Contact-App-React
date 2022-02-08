ea65b74edaac085257a2a511d306d105c2c223f7
Parikshit Hooda <phooda804@live.com>
2022-02-08 20:42:20 +0530

    MVP commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import AddPost from "./components/AddContact/index";
+import EditContact from "./components/EditContact/index";
+import Home from "./components/Home/index";
+import Navbar from "./components/Navbar/index";
 import "./styles.css";
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddPost />} />
+        <Route path="/edit/:id" element={<EditContact />} />
+      </Routes>
     </div>
   );
-}
+};
+export default App;
